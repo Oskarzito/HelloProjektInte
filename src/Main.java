dc6f193511b84118bc3e247792aded7f4a859f7c
Ludvig <luther467@hotmail.com>
2017-10-09 10:17:05 +0200

    ludvigs first push

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Skall satsa på en master inom Abstrakta maskiner och formella språk!");
+        System.out.println("självfallet eller master i trollkonst");
     }
 
 
