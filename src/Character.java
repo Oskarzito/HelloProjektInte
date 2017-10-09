1a2168144d91a06b87b0b393b42eab438c63cc01
Ludvig <luther467@hotmail.com>
2017-10-09 10:38:37 +0200

    ludvigs andra push

---
+++ src/Character.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Linnea on 2017-10-09.
+ */
+import java.util.*;
+public class Character {
+    private String name;
+    private ArrayList<Item> inventory;
+    private int level;
+
+    public Character(String name, int level){
+        this.name = name;
+        this.level = level;
+    }
+
+
+}
---
+++ src/Item.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Linnea on 2017-10-09.
+ */
+public class Item {
+    private String itemName;
+
+    public Item(String itemName){
+        this.itemName = itemName;
+    }
+}
