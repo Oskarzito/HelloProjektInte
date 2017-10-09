a658ffec36f052e820f2235b82dcd23479267b26
nigr0101 <32631684+nigr0101@users.noreply.github.com>
2017-10-09 10:53:12 +0200

    Test

---
+++ src/NicoleTest.java
@@ -0,0 +1,20 @@
+public class NicoleTest {
+    public class Dog {
+
+        private String name, race;
+        private int age;
+        private double weight;
+        private final double DACHSHUND_TAIL = 3.7;
+
+        public Dog(String name, String race, int age, double weight) {
+            this.name = name;
+            this.race = race;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
